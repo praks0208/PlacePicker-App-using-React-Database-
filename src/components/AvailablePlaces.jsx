9ce268406230bb84585f49a8614bc27b64b79c26
praks0208 <praks.sawant26@gmail.com>
2024-06-16 12:16:58 +0530

    Preparing the App For Data Fetching

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,12 @@
+
+
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+
+  const [avalablePlaces, setAvailablePlaces] = useState([]);
+
+
   return (
     <Places
       title="Available Places"
