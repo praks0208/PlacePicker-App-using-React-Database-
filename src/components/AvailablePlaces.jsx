5ea7f89b3e5596ce91df919a6a1ba1b872216030
praks0208 <praks.sawant26@gmail.com>
2024-06-16 13:35:48 +0530

    Transforming Fetched Data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
 import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -19,19 +20,27 @@ export default function AvailablePlaces({ onSelectPlace }) {
         if (!response.ok) {
           throw new Error('Failed to fetch places');
         }
-        setAvailablePlaces(resData.places);
-      } catch (error) {
-        setError({message:error.message || 'Could not fetch places, please try again'});
-      }
 
-      setIsFetching(false);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false); 
+        });
+      } catch (error) {
+        setError({
+          message:
+            error.message || 'Could not fetch places, please try again'
+        });
+        setIsFetching(false);
+      }
     }
 
     fetchPlaces();
   }, []);
 
-  if(error) {
-    return <Error title="An error occured" message={error.message}/>;
+  if (error) {
+    return <Error title="An error occured" message={error.message} />;
   }
 
   return (
